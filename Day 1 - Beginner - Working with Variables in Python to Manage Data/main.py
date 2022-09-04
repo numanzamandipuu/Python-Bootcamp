2ae409452ec13c739a0bbae643212a1ecad287e7
Numan Zaman Dipu <numanzamandipuu@gmail.com>
2022-09-04 20:19:09 +0600

    test subject 102

--- Day 1 - Beginner - Working with Variables in Python to Manage Data/main.py
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/main.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
