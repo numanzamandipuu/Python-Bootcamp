03d91492e70901fd3755a34a171d99f52deb9267
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-05 19:44:38 +0600

    input function final update

---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/06 - The Python Input Function/main.py
@@ -0,0 +1,6 @@
+from tkinter.tix import InputOnly
+
+
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name?"))
