e710aba459e1f4f272e797dd9224651e8c0abf7d
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-05 10:34:41 +0600

    sum file deleted

--- Day 1 - Beginner - Working with Variables in Python to Manage Data/main.py
+++
@@ -1,8 +0,0 @@
-def sum(a, b):
-    return (a + b)
-
-a = int(input('Enter 1st number: '))
-b = int(input('Enter 2nd number: '))
-
-print(f'Sum of {a} and {b} is {sum(a, b)}')
-print('Hello World!')
