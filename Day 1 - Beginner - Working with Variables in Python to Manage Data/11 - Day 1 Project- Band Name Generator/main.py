ffa4a820a7ee56954e617b0de29b58c71310c57a
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-06 00:50:39 +0600

    band name generator final update

--- Day 1 - Beginner - Working with Variables in Python to Manage Data/11 - Day 1 Project- Band Name Generator/main.py
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/11 - Day 1 Project- Band Name Generator/main.py
@@ -1,6 +1,4 @@
 print("Welcome to the Band Name Generator.")
-
-city = input("What's name of the city you grew up in?\n")
+street = input("What's name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
-
-print("Your band name could be " + city + " " + pet)
+print("Your band name could be " + street + " " + pet)
