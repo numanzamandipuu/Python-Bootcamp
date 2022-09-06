2404beb879eedb383022ca9f4fa3f6b0f0a7d614
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-06 12:12:58 +0600

    interactive data type final update

---
+++ Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/04 - [Interactive Coding Exercise] Data Types/main.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+first_digit = int(two_digit_number [0])
+second_digit = int(two_digit_number [1])
+
+two_digit_number = first_digit + second_digit
+
+print(two_digit_number)
