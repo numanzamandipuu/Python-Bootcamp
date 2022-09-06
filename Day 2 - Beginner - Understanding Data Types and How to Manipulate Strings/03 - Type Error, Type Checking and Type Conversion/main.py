10823bfee3b8dedaaaa9563e8455d69ebc0d3711
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-06 11:34:43 +0600

    data types final update

--- Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/03 - Type Error, Type Checking and Type Conversion/main.py
+++ Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/03 - Type Error, Type Checking and Type Conversion/main.py
@@ -1,3 +1,9 @@
-num_char = len(input("What's your name?"))
-new_num_char = str(num_char)
-print("Your name contains " + new_num_char + " characters.")
+# num_char = len(input("What's your name?"))
+# new_num_char = str(num_char)
+# print("Your name contains " + new_num_char + " characters.")
+
+a = float(123)
+print(type(a))
+
+# print(70 + float("100.5"))
+print(str(70) + str(100))
