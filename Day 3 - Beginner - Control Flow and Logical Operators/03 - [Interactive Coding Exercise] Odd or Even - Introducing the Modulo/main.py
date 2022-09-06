23e5c5680018440e53883f8ee3774980a255deb3
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-07 01:06:05 +0600

    modulo final update

---
+++ Day 3 - Beginner - Control Flow and Logical Operators/03 - [Interactive Coding Exercise] Odd or Even - Introducing the Modulo/main.py
@@ -0,0 +1,8 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
