49f6499fe7646495da35d5933eeb91787b66ab85
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-06 23:58:18 +0600

    rollercoaster update

---
+++ Day 3 - Beginner - Control Flow and Logical Operators/02 - Control Flow with if _ else and Conditional Operators/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
