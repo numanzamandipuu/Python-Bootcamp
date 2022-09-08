c89492f67520e9a9b887d86fe06474f231b326d4
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-08 12:24:13 +0600

    blueprint for pizza problem

---
+++ Day 3 - Beginner - Control Flow and Logical Operators/08 - [Interactive Coding Exercise] Pizza Order Practice/main.py
@@ -0,0 +1,8 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
