eeba78baca2c892ff8bf0c259d146253f523cf77
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-08 10:35:47 +0600

    multiple if statement final update

--- Day 3 - Beginner - Control Flow and Logical Operators/07 - Multiple If Statements in Succession/main.py
+++ Day 3 - Beginner - Control Flow and Logical Operators/07 - Multiple If Statements in Succession/main.py
@@ -1,14 +1,25 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
     if  age < 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Child tickets are $5.")
     elif age <= 18:
-        print("Please pay $7.")
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}")
+
 else:
     print("Sorry, you have to grow taller before you can ride.")
