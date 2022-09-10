4624034eaa709af9e899922020b4e24b7bb6ace5
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-10 12:12:32 +0600

    treasure island final update (my code)

--- Day 3 - Beginner - Control Flow and Logical Operators/11 - Day 3 Project- Treasure Island/main.py
+++ Day 3 - Beginner - Control Flow and Logical Operators/11 - Day 3 Project- Treasure Island/main.py
@@ -25,3 +25,31 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
+cross_road_input = input("You're at a crossroad. Where do you want to go? Type 'left' or 'right'\n")
+cross_road = cross_road_input.lower()
+if cross_road == "left":
+
+    lake_input = input("You've come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.\n")
+    lake = lake_input.lower()
+    if lake == "wait":
+
+        door_input = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n")
+        door = door_input.lower()
+        if door == "yellow":
+            print("You found the treasure! You Win!")
+        elif door == "red":
+            print("It's a room full of fire. Game Over.")
+        elif door == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+
+    elif lake == "swim":
+        print("You get attacked by an angry trout. Game Over.")
+    else:
+        print("Please choose a valid direction.")
+
+elif cross_road == "right":
+    print("You fell into a hole. Game Over.")
+else:
+    print("Please choose a valid direction.")
