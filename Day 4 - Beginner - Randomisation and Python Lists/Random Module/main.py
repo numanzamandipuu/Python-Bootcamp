e8d4ef251916ac7e2d7fe60ded99b0f276b7347b
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-10 22:07:03 +0600

    random module final update

---
+++ Day 4 - Beginner - Randomisation and Python Lists/Random Module/main.py
@@ -0,0 +1,10 @@
+import random
+
+randomIntegeter = random.randint(1 , 10)
+print(randomIntegeter)
+
+randomFloat = random.random() * 5
+print(randomFloat)
+
+love_score = random.randint(1 , 100)
+print(f"Your love score is {love_score}")
