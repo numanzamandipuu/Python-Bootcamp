e1e1278ca0c54ef4b2d635a19c322b0fce420267
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-11 22:49:29 +0600

    interactive bill pay blueprint

---
+++ Day 4 - Beginner - Randomisation and Python Lists/05 - [Interactive Coding Exercise] Banker Roulette - Who will pay the bill/main.py
@@ -0,0 +1,6 @@
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
