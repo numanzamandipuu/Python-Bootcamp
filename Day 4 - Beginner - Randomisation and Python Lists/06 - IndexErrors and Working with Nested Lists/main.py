7c69b66984501d657e3f8a145a4b605243e5b580
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-11 23:44:29 +0600

    nested list final update

--- Day 4 - Beginner - Randomisation and Python Lists/06 - IndexErrors and Working with Nested Lists/main.py
+++ Day 4 - Beginner - Randomisation and Python Lists/06 - IndexErrors and Working with Nested Lists/main.py
@@ -9,6 +9,12 @@
 # love_score = random.randint(1 , 100)
 # print(f"Your love score is {love_score}")
 
-states_of_america = ["Item01", "Item02", "Item03", "Item04", "Item05", "Item06", ]
+states_of_america = ["Item01", "Item02", "Item03", "Item04", "Item05", "Item06"]
 
 print(states_of_america)
+
+fruits = ["Item01", "Item02", "Item03"]
+vegetables = ["Item04", "Item05", "Item06"]
+
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
