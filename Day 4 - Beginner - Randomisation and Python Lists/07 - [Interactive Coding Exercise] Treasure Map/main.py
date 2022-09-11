681da56b6f0eed22c84da18891b399e34b1bb656
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-11 23:52:36 +0600

    treasure map source file part 01

---
+++ Day 4 - Beginner - Randomisation and Python Lists/07 - [Interactive Coding Exercise] Treasure Map/main.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
