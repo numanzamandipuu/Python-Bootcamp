2b9f621a43da3a00271be554429417277141539e
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-11 22:08:54 +0600

    python list final update

---
+++ Day 4 - Beginner - Randomisation and Python Lists/04 - Understanding the Offset and Appending Items to Lists/main.py
@@ -0,0 +1,18 @@
+# import random
+
+# randomIntegeter = random.randint(1 , 10)
+# print(randomIntegeter)
+
+# randomFloat = random.random() * 5
+# print(randomFloat)
+
+# love_score = random.randint(1 , 100)
+# print(f"Your love score is {love_score}")
+
+states_of_america = ["Item01", "Item02", "Item03", "Item04", "Item05", "Item06", ]
+
+states_of_america[1] = "Item002"
+
+states_of_america.extend(["Item07", "Item08"])
+
+print(states_of_america)
