9d8e0a697fd03c37156c13129755701a0fefd10e
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-13 14:29:42 +0600

    interactive avg height final update (my code)

--- Day 5 - Beginner - Python Loops/03 - [Interactive Coding Exercise] Average Height/main.py
+++ Day 5 - Beginner - Python Loops/03 - [Interactive Coding Exercise] Average Height/main.py
@@ -2,20 +2,17 @@
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
-# print(student_heights)
 # 🚨 Don't change the code above 👆
+
 
 #Write your code below this row 👇
 
-total_height = 0
-for height in student_heights:
-  total_height += height
-print(f"total height = {total_height}")
+total_heights = 0
+for heights in student_heights:
+    total_heights += heights
 
-number_of_students = 0
-for student in student_heights:
-  number_of_students += 1
-print(f"number of students = {number_of_students}")
-  
-average_height = round(total_height / number_of_students)
-print(average_height)
+total_students = 0
+for students in student_heights:
+    total_students += 1
+    
+print(round(total_heights / total_students))
