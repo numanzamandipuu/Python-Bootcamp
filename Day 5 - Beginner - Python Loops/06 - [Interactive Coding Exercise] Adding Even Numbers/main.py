ada90c6e10e31712818991404f5e4d668d8ea6af
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-13 23:52:17 +0600

    interactive even number update

---
+++ Day 5 - Beginner - Python Loops/06 - [Interactive Coding Exercise] Adding Even Numbers/main.py
@@ -0,0 +1,16 @@
+#Write your code below this row 👇
+
+even_sum = 0
+for number in range(2, 101, 2):
+  # print(number)
+  even_sum += number
+print(even_sum)
+  
+#or
+
+# alternative_sum = 0
+# for number in range(1, 101):
+#   if number % 2 == 0:
+#     # print(number)
+#     alternative_sum += number
+# print(alternative_sum)
