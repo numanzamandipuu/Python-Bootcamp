e24a7218ca8b9295c08f36c57a4323f1c18d7ff7
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-14 09:30:19 +0600

    loops and range function final update

---
+++ Day 5 - Beginner - Python Loops/05 - for loops and the range() Function/main.py
@@ -0,0 +1,14 @@
+# For Loop with Lists
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + "Pie")
+
+# For Loop with Range
+# for number in range(1, 11, 3):
+#     print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
