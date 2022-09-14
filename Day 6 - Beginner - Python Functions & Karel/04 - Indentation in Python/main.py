362691901f525a9ab3238b1c004e4e882d7fddd5
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-14 21:44:37 +0600

    indentation final update

---
+++ Day 6 - Beginner - Python Functions & Karel/04 - Indentation in Python/main.py
@@ -0,0 +1,13 @@
+# This is for avoiding errors
+
+sky = "sky"
+
+# Code starts from here
+
+def my_function():
+    if sky == "clear":
+        print("blue")
+    elif sky == "cloudy":
+        print("grey")
+    print("Hello")
+print("World")
