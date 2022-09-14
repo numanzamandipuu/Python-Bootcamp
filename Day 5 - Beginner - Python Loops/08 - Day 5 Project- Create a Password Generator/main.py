c8f6c20604fb681a9600d8c2c77fa9d5169af3af
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-14 14:08:20 +0600

    password generator final update (my code)

---
+++ Day 5 - Beginner - Python Loops/08 - Day 5 Project- Create a Password Generator/main.py
@@ -0,0 +1,22 @@
+#Password Generator Project
+import random
+from random import shuffle
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+random_letters = random.sample(letters, nr_letters)
+random_symbols = random.sample(symbols, nr_symbols)
+random_numbers = random.sample(numbers, nr_numbers)
+
+organized_password = random_letters + random_symbols + random_numbers
+random_password = shuffle(organized_password)
+
+for password in organized_password:
+    print(password, end='')
