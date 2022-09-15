033bb95db291142840a4e0b8cff66d833f3410ef
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-15 17:58:01 +0600

    challenge 02 final update (my code)

--- Day 7 - Beginner - Hangman/05 - Challenge 2 - Replacing Blanks with Guesses/main.py
+++ Day 7 - Beginner - Hangman/05 - Challenge 2 - Replacing Blanks with Guesses/main.py
@@ -1,26 +1,18 @@
-#Step 2
-
 import random
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
-#Testing code
 print(f'Pssst, the solution is {chosen_word}.')
-
-#TODO-1: - Create an empty List called display.
-#For each letter in the chosen_word, add a "_" to 'display'.
-#So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
-
 guess = input("Guess a letter: ").lower()
 
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+display = []
+for alphabet in chosen_word:
+    display == display.append("_")
 
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
+for n in range(word_length):
+    if guess == chosen_word[n]:
+        display[n] = guess
+
+print(display)
