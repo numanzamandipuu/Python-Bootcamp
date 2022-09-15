3e1a8ebc67b6a71b418cfe6efeaa7321a7c11aa3
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-15 14:52:06 +0600

    challenge 01 solution final update

--- Day 7 - Beginner - Hangman/04 - Challenge 1 Solution - How to Check the User's Answer/main.py
+++ Day 7 - Beginner - Hangman/04 - Challenge 1 Solution - How to Check the User's Answer/main.py
@@ -1,22 +1,17 @@
 #Step 1 
-import random
 
-word_list = ["ardvark", "baboon", "camel"]
+word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
+import random
 chosen_word = random.choice(word_list)
-word_list = list(chosen_word)
 
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ").lower()
 
-guess_word = input("Guess a letter: ").lower()
-guess = guess_word[0]
-
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
-for n in word_list:
-    if guess == n:
+# TODO-3 - Check if the letter the user guessed (guess) is one of the leters in the chosen_word.
+for letter in chosen_word:
+    if letter == guess:
         print("Right")
     else:
         print("Wrong")
