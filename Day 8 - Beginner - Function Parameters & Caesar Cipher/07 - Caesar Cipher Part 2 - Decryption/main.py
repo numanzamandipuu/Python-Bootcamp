e6611052abc3d545bfd5475e4c40b97b8c9f4a92
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-16 23:59:51 +0600

    ceasar cipher part 02 blueprint from my code

--- Day 8 - Beginner - Function Parameters & Caesar Cipher/07 - Caesar Cipher Part 2 - Decryption/main.py
+++ Day 8 - Beginner - Function Parameters & Caesar Cipher/07 - Caesar Cipher Part 2 - Decryption/main.py
@@ -1,17 +1,26 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The encoded text is {cipher_text}")
+def encrypt(text_letter, shift_ammount):
+    cipher_text = ""
 
+    for n in text_letter:
+        text_position = alphabet.index(n)
+        new_position = text_position + shift_ammount
+
+        if new_position > 25:
+            new_position = new_position - 26
+
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+
+    print(f"The encoded text is {cipher_text}")
+
+
+encrypt(text_letter = text, shift_ammount = shift)
 #TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
 
   #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
@@ -23,4 +32,3 @@ def encrypt(plain_text, shift_amount):
 
 
 #TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
-encrypt(plain_text=text, shift_amount=shift)
