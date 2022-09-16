f5c7889f4ccb71146011f7ed14d8efaafa611469
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-16 12:54:05 +0600

    functions with input final update

---
+++ Day 8 - Beginner - Function Parameters & Caesar Cipher/02 - Functions with Inputs/main.py
@@ -0,0 +1,14 @@
+# Simple Function
+# def greet():
+#     print("Hello Dipu")
+#     print("How do you do Dipu?")
+#     print("Isn't the weather nice today?")
+# greet()
+
+# Function that allows for input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+greet_with_name("Dipu")
