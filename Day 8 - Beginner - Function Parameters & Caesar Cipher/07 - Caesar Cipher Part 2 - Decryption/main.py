9dea9b55d9766060776afe29902549dccbdb912e
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-17 00:45:07 +0600

    ceasar cipher part 02 (my code) (highly modified)

--- Day 8 - Beginner - Function Parameters & Caesar Cipher/07 - Caesar Cipher Part 2 - Decryption/main.py
+++ Day 8 - Beginner - Function Parameters & Caesar Cipher/07 - Caesar Cipher Part 2 - Decryption/main.py
@@ -1,34 +1,45 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-def encrypt(text_letter, shift_ammount):
-    cipher_text = ""
-
-    for n in text_letter:
-        text_position = alphabet.index(n)
-        new_position = text_position + shift_ammount
-
-        if new_position > 25:
-            new_position = new_position - 26
-
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
-
-    print(f"The encoded text is {cipher_text}")
+if direction == "encode" or direction == "decode":
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+else:
+    print("Please pick a valid keyword.")
 
 
-encrypt(text_letter = text, shift_ammount = shift)
-#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(text_encrypt, shift_encrypt):
+    cipher_text_en = ""
 
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
-  #e.g. 
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
+    for n in text_encrypt:
+        text_position_en = alphabet.index(n)
+        new_position_en = text_position_en + shift_encrypt
+
+        if new_position_en > 25:
+            new_position_en -= 26
+
+        new_letter_en = alphabet[new_position_en]
+        cipher_text_en += new_letter_en
+    print(f"The encoded text is {cipher_text_en}")
 
 
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+def decrypt(text_decrypt, shift_decrypt):
+    cipher_text_de = ""
+
+    for n in text_decrypt:
+        text_position_de = alphabet.index(n)
+        new_position_de = text_position_de - shift_decrypt
+
+        if new_position_de < 0:
+            new_position_de += 26
+
+        new_letter_de = alphabet[new_position_de]
+        cipher_text_de += new_letter_de
+    print(f"The decoded text is {cipher_text_de}")
+
+
+if direction == "encode":
+    encrypt(text_encrypt = text, shift_encrypt = shift)
+elif direction == "decode":
+    decrypt(text_decrypt = text, shift_decrypt = shift)
