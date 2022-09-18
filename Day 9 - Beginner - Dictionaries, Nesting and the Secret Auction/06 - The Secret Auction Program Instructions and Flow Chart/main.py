0a452aae8d0238a67b661134a4431835566e4968
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-18 23:44:14 +0600

    secret auction program part 02 SAP02

--- Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/06 - The Secret Auction Program Instructions and Flow Chart/main.py
+++ Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/06 - The Secret Auction Program Instructions and Flow Chart/main.py
@@ -5,23 +5,23 @@ clear = lambda: os.system("cls")
 print(art.logo)
 auction = {}
 stop_auction = False
-
-key = input("What is your name?: ")
-value = int(input("What is your bid?: $"))
-auction[key] = value
-question = input("Are there any other bidders? Type 'yes or 'no'.\n").lower()
+question = "yes"
 
 while stop_auction == False:
     if question == "yes":
-        clear()
         key = input("What is your name?: ")
         value = int(input("What is your bid?: $"))
         auction[key] = value
         question = input("Are there any other bidders? Type 'yes or 'no'.\n").lower()
+        if question == "yes":
+            clear()     
     elif question == "no":
         stop_auction = True
     else:
         print("You've picked an invalid keyword.\nEnd of the auction.")
         stop_auction = True
 
-print(auction)
+winner = max(auction, key=auction.get)
+amount = auction[winner]
+
+print(f"The winner is {winner} with a bid of ${amount}")
