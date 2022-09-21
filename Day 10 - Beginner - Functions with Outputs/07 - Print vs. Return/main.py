d00fab1c6aef0b325bd5063513a800d5599cb711
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-21 17:51:27 +0600

    print vs. return final update

--- Day 10 - Beginner - Functions with Outputs/07 - Print vs. Return/main.py
+++ Day 10 - Beginner - Functions with Outputs/07 - Print vs. Return/main.py
@@ -28,6 +28,13 @@ operations_symbol = input("Pick an operation from the line above: ")
 num2 = int(input("What's the second number?: "))
 
 calculator_function = operations[operations_symbol]
-answer = calculator_function(num1, num2)
+first_answer = calculator_function(num1, num2)
 
-print(f"{num1} {operations_symbol} {num2} = {answer}")
+print(f"{num1} {operations_symbol} {num2} = {first_answer}")
+
+operation_symbol = input("Pick another operation: ") 
+num3 = int(input("What's the next number?: "))
+calculation_function = operations[operation_symbol] 
+second_answer = calculation_function(calculation_function(num1, num2), num3)
+
+print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
