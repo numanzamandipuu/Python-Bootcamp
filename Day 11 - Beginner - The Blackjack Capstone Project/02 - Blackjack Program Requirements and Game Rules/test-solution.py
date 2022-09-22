518e2e9f036a6a3c7c6b223392d4d8ae2b4e2464
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-22 21:41:51 +0600

    my_card & computer_card with sum

--- Day 11 - Beginner - The Blackjack Capstone Project/02 - Blackjack Program Requirements and Game Rules/test-solution.py
+++ Day 11 - Beginner - The Blackjack Capstone Project/02 - Blackjack Program Requirements and Game Rules/test-solution.py
@@ -1,13 +1,26 @@
 import os
+import random
 clear = lambda: os.system("cls")
 
-start = True
-while start:
-    start_input = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-    if start_input == "y":
-        start = False
-    elif start_input == "n":
-        start = False
-    else:
-        clear()
-        print("Please pick a valid keyword.")
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+my_card = random.sample(cards, 2)
+sum_my_card = sum(my_card)
+
+print(f"Your cards: {my_card}, current score: {sum_my_card}")
+
+computer_card = random.sample(cards, 2)
+print(f"Computer's first card: {computer_card[0]}")
+print(computer_card)
+
+
+
+# start = True
+# while start:
+#     start_input = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+#     if start_input == "y":
+#         start = False
+#     elif start_input == "n":
+#         start = False
+#     else:
+#         clear()
+#         print("Please pick a valid keyword.")
