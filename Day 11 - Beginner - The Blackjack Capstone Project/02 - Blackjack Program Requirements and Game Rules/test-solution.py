4b9190f8d3a5cc98307d22607ab4a36adcde3a0f
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-22 23:33:46 +0600

    yn_input done, result remaining

--- Day 11 - Beginner - The Blackjack Capstone Project/02 - Blackjack Program Requirements and Game Rules/test-solution.py
+++ Day 11 - Beginner - The Blackjack Capstone Project/02 - Blackjack Program Requirements and Game Rules/test-solution.py
@@ -4,27 +4,42 @@ clear = lambda: os.system("cls")
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 my_card = random.sample(cards, 2)
-sum_my_card = sum(my_card)
 
-# print(f"Your cards: {my_card}, current score: {sum_my_card}")
-
+# Computer card start
 computer_card = random.sample(cards, 2)
-print(computer_card)
 while sum(computer_card) < 17:
     add_computer_card = random.sample(cards, 1)
     if add_computer_card == [11] and sum(computer_card) > 10:
         add_computer_card = [1]
     computer_card.extend(add_computer_card)
+computer_card_sum = sum(computer_card)
+# Computer card end
 
-print(computer_card)
+def print_function():
+    sum_my_card = sum(my_card)
+    print(f"Your cards: {my_card}, current score: {sum_my_card}")
+    print(f"Computer's first card: {computer_card[0]}")
 
-# print(f"Computer's first card: {computer_card[0]}")
-# yn_input = input("Type 'y' to get another card, type 'n' to pass: ")
 
-# if yn_input == "y":
-#     add_card = random.sample(cards, 1)
-#     my_card.extend(add_card)
-#     print(f"Your cards: {my_card}, current score: {sum_my_card}")
+print_function()
+yn_input = input("Type 'y' to get another card, type 'n' to pass: ")
+
+if yn_input == "y":
+    looping = True
+else:
+    looping = False
+
+while looping == True:
+    add_card = random.sample(cards, 1)
+    my_card.extend(add_card)
+    print_function()
+    yn_input = input("Type 'y' to get another card, type 'n' to pass: ")
+    if yn_input == "y":
+        looping = True
+    else:
+        looping = False
+
+
 
 # start = True
 # while start:
