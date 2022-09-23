24584642d7099c1ba9406dca63a3e30917923901
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-23 10:09:19 +0600

    hint 04 and 05 solution final update

--- Day 11 - Beginner - The Blackjack Capstone Project/03 - Hint 4 & 5 Solution Walkthrough/main.py
+++ Day 11 - Beginner - The Blackjack Capstone Project/03 - Hint 4 & 5 Solution Walkthrough/main.py
@@ -33,11 +33,21 @@
 
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+
+def deal_card():
+    """Returns a random card form the deck."""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
 
 #Hint 6: Create a function called calculate_score() that takes a List of cards as input 
 #and returns the score. 
