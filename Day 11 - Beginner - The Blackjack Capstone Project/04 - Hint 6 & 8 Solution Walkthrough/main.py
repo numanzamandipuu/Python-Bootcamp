0d5d8e9b79d100b217293f7571d104c44cb798c6
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-23 10:20:37 +0600

    hint 06 to 08 solution final update

--- Day 11 - Beginner - The Blackjack Capstone Project/04 - Hint 6 & 8 Solution Walkthrough/main.py
+++ Day 11 - Beginner - The Blackjack Capstone Project/04 - Hint 6 & 8 Solution Walkthrough/main.py
@@ -52,10 +52,17 @@ for _ in range(2):
 #Hint 6: Create a function called calculate_score() that takes a List of cards as input 
 #and returns the score. 
 #Look up the sum() function to help you do this.
+def calculate_score(cards):
+    #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
 
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+    #Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
 
-#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+    return sum(cards)
 
 #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
 
