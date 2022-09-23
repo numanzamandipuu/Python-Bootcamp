7c549a42b0a34e6130e1d9653cc6e9b2fd129595
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-23 22:47:35 +0600

    hint 10 to 12 final update

--- Day 11 - Beginner - The Blackjack Capstone Project/06 - Hint 10 - 12 Solution Walkthrough/main.py
+++ Day 11 - Beginner - The Blackjack Capstone Project/06 - Hint 10 - 12 Solution Walkthrough/main.py
@@ -33,6 +33,7 @@
 
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
+
 import random
 
 def deal_card():
@@ -45,14 +46,19 @@ def deal_card():
 #Hint 6: Create a function called calculate_score() that takes a List of cards as input 
 #and returns the score. 
 #Look up the sum() function to help you do this.
+
 def calculate_score(cards):
     """Take a list of cards and return the score calculated from the cards"""
 
+
     #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+
     if sum(cards) == 21 and len(cards) == 2:
         return 0
 
+
     #Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+
     if 11 in cards and sum(cards) > 21:
         cards.remove(11)
         cards.append(1)
@@ -61,6 +67,7 @@ def calculate_score(cards):
 
 
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
+
 user_cards = []
 computer_cards = []
 is_game_over = False
@@ -97,6 +104,7 @@ while not is_game_over:
 while computer_score != 0 and computer_score < 17:
     computer_cards.append(deal_card())
     computer_score = calculate_score(computer_cards)
+    
 
 #Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
 
