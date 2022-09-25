652274ca8448f7c868afccf72004f23f604f4922
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-25 22:05:11 +0600

    modifying global scope final update

--- Day 12  - Beginner - Scope & Number Guessing Game/03 - How to Modify a Global Variable/main.py
+++ Day 12  - Beginner - Scope & Number Guessing Game/03 - How to Modify a Global Variable/main.py
@@ -40,3 +40,15 @@ def create_enemy():
         new_enemy = enemies[0]
 
     print(new_enemy)
+
+
+# Modifying Global Scope
+
+enemies = 1
+
+def increase_enemies():
+    print(f"enemies inside function: {enemies}")
+    return enemies + 1
+
+enemies = increase_enemies()
+print(f"enemies outside function: {enemies}")
