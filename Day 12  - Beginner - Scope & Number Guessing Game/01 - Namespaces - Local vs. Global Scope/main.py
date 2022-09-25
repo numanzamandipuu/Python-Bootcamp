f36f33307fadbf46eff63e0e18f3816f32695769
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-25 17:54:46 +0600

    namespace and scope final update

---
+++ Day 12  - Beginner - Scope & Number Guessing Game/01 - Namespaces - Local vs. Global Scope/main.py
@@ -0,0 +1,31 @@
+################### Scope ####################
+
+enemies = 1
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+# Local Scope
+
+def drink_potion():
+    potion_strength = 2
+    print(potion_strength)
+
+drink_potion()
+# print(potion_strength)
+
+# Global Scope
+player_health = 10
+
+def game():
+    def drink_potion():
+        potion_strength = 2
+        print(player_health)
+
+    drink_potion()
+
+print(player_health)
