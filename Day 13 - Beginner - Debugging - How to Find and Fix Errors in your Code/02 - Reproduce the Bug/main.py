fde492755547cf70e5a323095a8fb1a139cfcdb3
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-26 22:24:49 +0600

    reproducing the bug final update

--- Day 13 - Beginner - Debugging - How to Find and Fix Errors in your Code/02 - Reproduce the Bug/main.py
+++ Day 13 - Beginner - Debugging - How to Find and Fix Errors in your Code/02 - Reproduce the Bug/main.py
@@ -1,17 +1,17 @@
 ############DEBUGGING#####################
 
-# Describe Problem
-def my_function():
-  for i in range(1, 21):
-    if i == 20:
-      print("You got it")
-my_function()
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 21):
+#     if i == 20:
+#       print("You got it")
+# my_function()
 
-# # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
-# print(dice_imgs[dice_num])
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
