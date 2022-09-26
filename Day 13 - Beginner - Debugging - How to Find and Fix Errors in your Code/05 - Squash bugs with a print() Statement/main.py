085a031c016f6d6642a9865fecbdb2e07017e64a
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-26 23:16:21 +0600

    squash bugs with print() function final update

--- Day 13 - Beginner - Debugging - How to Find and Fix Errors in your Code/05 - Squash bugs with a print() Statement/main.py
+++ Day 13 - Beginner - Debugging - How to Find and Fix Errors in your Code/05 - Squash bugs with a print() Statement/main.py
@@ -20,20 +20,22 @@
 # elif year >= 1994:
 #   print("You are a Gen Z.")
 
-# Fix the Errors
-age = int(input("How old are you?"))
-if age > 18:
-  print(f"You can drive at age {age}.")
+# # Fix the Errors
+# age = int(input("How old are you?"))
+# if age > 18:
+#   print(f"You can drive at age {age}.")
 
-# #Print is Your Friend
-# pages = 0
-# word_per_page = 0
-# pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
-# total_words = pages * word_per_page
-# print(total_words)
+# Print is Your Friend
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(f"pages = {pages}")
+print(f"word_per_page = {word_per_page}")
+print(total_words)
 
-# #Use a Debugger
+# # Use a Debugger
 # def mutate(a_list):
 #   b_list = []
 #   for item in a_list:
