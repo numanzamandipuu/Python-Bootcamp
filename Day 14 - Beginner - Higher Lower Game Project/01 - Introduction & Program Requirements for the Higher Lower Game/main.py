13d905a1b634ecc3be96f9218ef9e70c48dbf4a0
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-27 20:00:08 +0600

    structure done for the higher lower game

--- Day 14 - Beginner - Higher Lower Game Project/01 - Introduction & Program Requirements for the Higher Lower Game/main.py
+++ Day 14 - Beginner - Higher Lower Game Project/01 - Introduction & Program Requirements for the Higher Lower Game/main.py
@@ -1,35 +1,55 @@
 import random
 import game_data
 import art
+import os
+clear = lambda: os.system("cls")
 
-# print(game_data.data[10]['description'])
 
-# print(art.logo)
+print(art.logo)
+game_over = False
+score = 0
+list_of_index = list(range(0, len(game_data.data)))
+compare_A_number = random.choice(list_of_index)
 
-# Comparison number detection
-compare_A_number = random.choice(list(range(0, len(game_data.data))))
-compare_B_whole_number = list(range(0, len(game_data.data)))
-compare_B_whole_number.remove(compare_A_number)
-compare_B_number = random.choice(compare_B_whole_number)
+while game_over == False:
 
-# All the functions
-def compare_A(key):
-    return game_data.data[compare_A_number][key]
-def compare_B(key):
-    return game_data.data[compare_B_number][key]
 
-def comparison():
-    if compare_A('follower_count') > compare_B('follower_count'):
-        return "a"
-    elif compare_A('follower_count') < compare_B('follower_count'):
-        return "b"
+    compare_B_list = []
+    for n in list_of_index:
+        if n != compare_A_number:
+            compare_B_list.append(n)
+    compare_B_number = random.choice(compare_B_list)
 
-print(f"Compare A: {compare_A('name')}, a {compare_A('description')}, from {compare_A('country')}.")
-print(f"Compare B: {compare_B('name')}, a {compare_B('description')}, from {compare_B('country')}.")
 
-compare_input = input("Who has more followers? Type 'A' or 'B': ").lower()
+    # All the functions
+    def compare_A(key):
+        return game_data.data[compare_A_number][key]
 
-if comparison() == compare_input:
-    print("right madafaka")
-elif comparison() != compare_input:
-    print("you fuckin donkey")
+    def compare_B(key):
+        return game_data.data[compare_B_number][key]
+
+    def comparison():
+        if compare_A('follower_count') > compare_B('follower_count'):
+            return "a"
+        elif compare_A('follower_count') < compare_B('follower_count'):
+            return "b"
+
+    Compare_A = f"{compare_A('name')}, a {compare_A('description')}, from {compare_A('country')}."
+    Compare_B = f"{compare_B('name')}, a {compare_B('description')}, from {compare_B('country')}."
+
+    print(f"Compare A: {Compare_A}")
+    print(art.vs)
+    print(f"Against B: {Compare_B}")
+    compare_input = input("Who has more followers? Type 'A' or 'B': ").lower()
+    if comparison() == compare_input:
+        score += 1
+        clear()
+        print(art.logo)
+        print(f"You're right! Current score: {score}.")
+        compare_A_number = compare_B_number
+
+    elif comparison() != compare_input:
+        clear()
+        print(art.logo)
+        print(f"Sorry, that's wrong. Final score: {score}")
+        game_over = True
