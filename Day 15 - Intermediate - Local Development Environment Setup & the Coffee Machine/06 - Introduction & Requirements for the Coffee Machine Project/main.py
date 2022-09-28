c65fc231d5dbadb590a3678d73b120dfbfa80eb8
numanzamandipuu <numanzamandipuu@gmail.com>
2022-09-29 01:24:24 +0600

    report section almost done

--- Day 15 - Intermediate - Local Development Environment Setup & the Coffee Machine/06 - Introduction & Requirements for the Coffee Machine Project/main.py
+++ Day 15 - Intermediate - Local Development Environment Setup & the Coffee Machine/06 - Introduction & Requirements for the Coffee Machine Project/main.py
@@ -3,6 +3,14 @@ from data import MENU, resources
 # print(MENU[command]["cost"])
 # print(MENU["espresso"]["ingredients"]["coffee"])
 
+# Creating a list for report
+water_amount = resources["water"]
+milk_amount = resources["milk"]
+coffee_amount = resources["coffee"]
+money = 0
+coffee_off = False
+
+
 # Function for coin transaction
 def coins(command):
     print("Please insert coins.")
@@ -14,15 +22,33 @@ def coins(command):
     return round(total - MENU[command]["cost"], 2)
 
 
-command = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
+while coffee_off == False:
 
-if command == "espresso" or command == "latte" or command == "cappuccino":
-    print(f"Here is ${coins(command)} in change.")
-elif command == "report":
-    print(resources)
-elif command == "off":
-    exit()
-else:
-    print("Please pick a valid keyword.")
+    command = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+    report_list = [
+        f"Water: {water_amount}ml",
+        f"Milk: {milk_amount}ml",
+        f"Coffee: {coffee_amount}g",
+        f"Money: ${money}"
+    ]
+
+    if command == "espresso" or command == "latte" or command == "cappuccino":
+
+        print(f"Here is ${coins(command)} in change.")
+
+        water_amount -= MENU[command]["ingredients"]["water"]
+        if command != "espresso":
+            milk_amount -= MENU[command]["ingredients"]["milk"]
+        coffee_amount -= MENU[command]["ingredients"]["coffee"]
+        money += MENU[command]["cost"]
+
+    elif command == "report":
+        for n in report_list:
+            print(n)
+    elif command == "off":
+        exit()
+    else:
+        print("Please pick a valid keyword.")
 
