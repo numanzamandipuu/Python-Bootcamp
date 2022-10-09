cb49f100c45825228c85e9903c3f21a7abbde697
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-09 19:41:45 +0600

    main functionality added to main.py

--- Day 16  - Intermediate - Object Oriented Programming (OOP)/06 - Building the Coffee Machine in OOP/main.py
+++ Day 16  - Intermediate - Object Oriented Programming (OOP)/06 - Building the Coffee Machine in OOP/main.py
@@ -1,3 +1,22 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+items = menu.get_items()
+
+while is_on == True:
+    choice = input(f"What would you like? ({items}): ").lower()
+    if choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif choice == "off":
+        is_on = False
+    elif choice == "espresso" or choice == "latte" or choice == "cappuccino":
+        print("")
+        is_on = False
+    else:
+        print("Please pick a valid keyword.")
