1f74daa15d6fa73d4d4c300186f25ce743ae93d4
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-10 22:26:01 +0600

    __init__() function final update

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/03 - Working with Attributes, Class Constructors and the __init__() Function/main.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/03 - Working with Attributes, Class Constructors and the __init__() Function/main.py
@@ -1,4 +1,12 @@
 class User:
-    pass
+    
+    def __init__(self, user_id, username):
+        self.user_id = user_id
+        self.username = username
+        self.followers = 0
 
-user_1 = User()
+
+user_1 = User("001", "angela")
+user_2 = User("002", "jack")
+
+print(user_1.followers)
