f1a083b426534ba40367843f5d7ad86e8fb2b143
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-10 17:28:22 +0600

    modified code for coffee machine final update

---
+++ Day 16  - Intermediate - Object Oriented Programming (OOP)/07 - Walkthrough and Solution for the OOP Coffee Machine/The Coffee Machine Project (My Code).py
@@ -0,0 +1,21 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+items = menu.get_items()
+
+while is_on == True:
+    choice = input(f"What would you like? ({items}): ").lower()
+    if choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif choice == "off":
+        is_on = False
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
