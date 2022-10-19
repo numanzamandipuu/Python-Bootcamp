0c15f199a7a5b71c99ca3ad859d47e9c82a39133
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-19 22:22:17 +0600

    question model  added from scratch

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/04 - Adding Methods to a Class/main.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/04 - Adding Methods to a Class/main.py
@@ -1,4 +1,5 @@
 class User:
+    
     def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
---
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/05 - Quiz Project Part 1 - Creating the Question Class/question_model.py
@@ -0,0 +1,8 @@
+from mimetypes import init
+
+
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
