993c8a4fb87c9cdf09d5397955424726840089a9
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-19 22:35:37 +0600

    for loop applied in question bank list

---
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/06 - Quiz Project Part 2 - Creating the List of Question Objects from the Data/main.py
@@ -0,0 +1,11 @@
+from data import question_data
+from question_model import Question
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+print(question_bank[0].answer)
