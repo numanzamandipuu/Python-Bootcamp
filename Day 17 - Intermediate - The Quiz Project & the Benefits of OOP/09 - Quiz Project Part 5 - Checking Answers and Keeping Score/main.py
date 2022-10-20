0aacef400b1281c103e3d913233d5dc7081d84ff
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-20 19:08:19 +0600

    checking answer final update

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/09 - Quiz Project Part 5 - Checking Answers and Keeping Score/main.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/09 - Quiz Project Part 5 - Checking Answers and Keeping Score/main.py
@@ -13,3 +13,6 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_question():
     quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
