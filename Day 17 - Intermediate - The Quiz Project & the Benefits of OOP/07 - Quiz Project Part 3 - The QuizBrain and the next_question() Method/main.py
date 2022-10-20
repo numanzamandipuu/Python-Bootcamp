552404b5f45b5b6635fe3b9ce4e6231ac7da11d3
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-20 15:42:46 +0600

    quizbrain & the next question method final update

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/07 - Quiz Project Part 3 - The QuizBrain and the next_question() Method/main.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/07 - Quiz Project Part 3 - The QuizBrain and the next_question() Method/main.py
@@ -1,5 +1,6 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -8,4 +9,5 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-print(question_bank[0].answer)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
