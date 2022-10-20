ab3b4eea9b3884cec572ac8d2d936f501b785837
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-20 19:48:27 +0600

    benefits of oop - opentdb final update

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/10 - The Benefits of OOP - Use Open Trivia DB to Get New Questions/main.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/10 - The Benefits of OOP - Use Open Trivia DB to Get New Questions/main.py
@@ -4,8 +4,8 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
