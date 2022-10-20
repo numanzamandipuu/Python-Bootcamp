a9e530d984d7d0f93f73181aeb3ad95a0a2c1683
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-20 15:48:47 +0600

    continue showing new question blueprint

---
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/08 - Quiz Project Part 4 - How to Continue Showing New Questions/main.py
@@ -0,0 +1,13 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
