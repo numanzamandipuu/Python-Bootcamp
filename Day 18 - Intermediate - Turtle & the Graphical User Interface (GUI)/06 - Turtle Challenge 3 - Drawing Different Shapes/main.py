38b022f86556fd16e421fd09fec15ec07d7d97de
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-22 21:41:38 +0600

    identical source code added to turtle challenge 03

--- Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/06 - Turtle Challenge 3 - Drawing Different Shapes/main.py
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/06 - Turtle Challenge 3 - Drawing Different Shapes/main.py
@@ -9,8 +9,29 @@ while num_sides < 11:
     for n in range(num_sides):
         tim.forward(100)
         tim.right(360 / num_sides)
-        
+
     num_sides += 1
 
 screen = Screen()
 screen.exitonclick()
+
+
+###### identical source code start ######
+
+# import turtle as t
+# import random
+
+# tim = t.Turtle()
+# colors = ["navy", "yellow", "firebrick", "magenta", "purple", "red", "blue", "black", "green", "indigo"]
+
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for i in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+
+# for n in range(3, 11):
+#     tim.color(random.choice(colors))
+#     draw_shape(n)
+
+###### identical source code end ######
