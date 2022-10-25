942cbdeeb3502179ba0f262660482525ac6bc26f
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-25 21:28:25 +0600

    extract rgb from input (my code using for loop)

---
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/10 - The Hirst Painting Project Part 1 - How to Extract RGB Values from Images/main.py
@@ -0,0 +1,18 @@
+# import colorgram
+
+# color = colorgram.extract('Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/10 - The Hirst Painting Project Part 1 - How to Extract RGB Values from Images/image.jpg', 30)
+
+# color_list = []
+
+# for i in range(30):
+#     color_list_item = color[i]
+#     r = color_list_item.rgb[0]
+#     g = color_list_item.rgb[1]
+#     b = color_list_item.rgb[2]
+#     color_tuple = (r, g, b)
+#     color_list.append(color_tuple)
+
+# print(color_list)
+
+
+color_list = [(202, 164, 110), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
