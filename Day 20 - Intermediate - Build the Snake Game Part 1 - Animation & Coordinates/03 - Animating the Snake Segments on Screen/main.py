a25e353113ac14d5eb01442a4149da959113fcfd
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-27 17:16:02 +0600

    deleted identical of animating snake (won't add)

--- Day 20 - Intermediate - Build the Snake Game Part 1 - Animation & Coordinates/03 - Animating the Snake Segments on Screen/main.py
+++ Day 20 - Intermediate - Build the Snake Game Part 1 - Animation & Coordinates/03 - Animating the Snake Segments on Screen/main.py
@@ -34,26 +34,3 @@ while game_is_on:
         
 
 screen.exitonclick()
-
-
-
-###### identical source code start ######
-
-# from turtle import Turtle, Screen
-
-# screen = Screen()
-# screen.setup(height=600, width=600)
-# screen.bgcolor("black")
-# screen.title("My Snake Game")
-
-# starting_position = [(0, 0), (-20, 0), (-40, 0)]
-
-# for position in starting_position:
-#     new_segment = Turtle("square")
-#     new_segment.color("white")
-#     new_segment.goto(position)
-
-
-# screen.exitonclick()
-
-###### identical source code end ######
