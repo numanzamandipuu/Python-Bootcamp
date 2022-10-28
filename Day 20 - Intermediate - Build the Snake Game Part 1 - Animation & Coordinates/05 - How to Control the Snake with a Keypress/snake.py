104d94d526384bd387b7b0969bae009a5a671014
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-28 12:46:12 +0600

    blueprint of D20L05

---
+++ Day 20 - Intermediate - Build the Snake Game Part 1 - Animation & Coordinates/05 - How to Control the Snake with a Keypress/snake.py
@@ -0,0 +1,40 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+
+class Snake:
+
+    def __init__(self):
+        self.segment = []
+        self.create_snake()
+
+
+    def create_snake(self):
+        for i in range(3):
+            tim = Turtle("square")
+            tim.color("white")
+            tim.penup()
+            tim.goto(x=(-20 * i), y=0)
+            self.segment.append(tim)
+
+
+    def move(self):
+        for n in range((len(self.segment) - 1), 0, -1):
+            new_x = self.segment[n - 1].xcor()
+            new_y = self.segment[n - 1].ycor()
+            self.segment[n].goto(new_x, new_y)
+
+        self.segment[0].forward(MOVE_DISTANCE)
+
+
+    def up(self):
+        self.segment[0].setheading(90)
+
+    def down(self):
+        self.segment[0].setheading(270)
+
+    def left(self):
+        self.segment[0].setheading(180)
+
+    def right(self):
+        self.segment[0].setheading(0)
