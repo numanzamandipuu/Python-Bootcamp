104f7ac37d97b34888d018d7829f2d4eebfda32a
numanzamandipuu <numanzamandipuu@gmail.com>
2022-10-30 10:05:52 +0600

    random food is now fully functional

--- Day 21 - Intermediate - Build the Snake Game Part 2 - Inheritance & List Slicing/03 - Detect Collisions with Food/main.py
+++ Day 21 - Intermediate - Build the Snake Game Part 2 - Inheritance & List Slicing/03 - Detect Collisions with Food/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkeypress(fun=snake.up, key="w")
