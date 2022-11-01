e8f08654cf843c3fe69d81876af3b3c53e7332f3
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-01 22:57:34 +0600

    creating a paddle my attempt (unsuccessful)

--- Day 22 - Intermediate - Build Pong - The Famous Arcade Game/03 - Create a Paddle That Responds to Key Presses/main.py
+++ Day 22 - Intermediate - Build Pong - The Famous Arcade Game/03 - Create a Paddle That Responds to Key Presses/main.py
@@ -1,11 +1,31 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
+right_tim = Turtle()
 screen = Screen()
+
+
+def right_tim_up():
+    right_tim.forward(20)
+
+def right_tim_down():
+    right_tim.forward(-20)
+
 
 screen.setup(height= 600, width= 800)
 screen.bgcolor("black")
 screen.title("Pong")
+
+
+right_tim.goto(x= 350, y=0)
+right_tim.shape("square")
+right_tim.penup()
+right_tim.color("white")
+right_tim.turtlesize(stretch_wid= 5, stretch_len=1)
+
+
+screen.listen()
+screen.onkeypress(right_tim_up, "i")
+screen.onkeypress(right_tim_down, "k")
 
 
 screen.exitonclick()
