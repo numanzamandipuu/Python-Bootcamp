e8643198d2e61d38e69bdda0e1ba25f8c45b6ebf
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-01 23:22:03 +0600

    paddle rename and while loop added for screen

--- Day 22 - Intermediate - Build Pong - The Famous Arcade Game/03 - Create a Paddle That Responds to Key Presses/main.py
+++ Day 22 - Intermediate - Build Pong - The Famous Arcade Game/03 - Create a Paddle That Responds to Key Presses/main.py
@@ -1,33 +1,39 @@
 from turtle import Turtle, Screen
 
-right_tim = Turtle()
+paddle = Turtle()
 screen = Screen()
 
 
-def right_tim_up():
-    ycor_up = right_tim.position()[1] + 20
-    right_tim.goto(x= 350, y= ycor_up)
+def up():
+    ycor_up = paddle.position()[1] + 20
+    paddle.goto(x= 350, y= ycor_up)
 
-def right_tim_down():
-    ycor_down = right_tim.position()[1] - 20
-    right_tim.goto(x= 350, y= ycor_down)
+def down():
+    ycor_down = paddle.position()[1] - 20
+    paddle.goto(x= 350, y= ycor_down)
 
 
 screen.setup(height= 600, width= 800)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
 
-right_tim.goto(x= 350, y=0)
-right_tim.shape("square")
-right_tim.penup()
-right_tim.color("white")
-right_tim.turtlesize(stretch_wid= 5, stretch_len=1)
+paddle.goto(x= 350, y=0)
+paddle.shape("square")
+paddle.penup()
+paddle.color("white")
+paddle.turtlesize(stretch_wid= 5, stretch_len=1)
 
 
 screen.listen()
-screen.onkeypress(right_tim_up, "i")
-screen.onkeypress(right_tim_down, "k")
+screen.onkeypress(up, "i")
+screen.onkeypress(down, "k")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 screen.exitonclick()
