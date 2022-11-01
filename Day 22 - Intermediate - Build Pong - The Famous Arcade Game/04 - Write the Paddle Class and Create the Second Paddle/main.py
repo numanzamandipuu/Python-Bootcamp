0210caf62086ed485548c8b28b6c45a13ed080bc
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-01 23:58:47 +0600

    paddle class created

--- Day 22 - Intermediate - Build Pong - The Famous Arcade Game/04 - Write the Paddle Class and Create the Second Paddle/main.py
+++ Day 22 - Intermediate - Build Pong - The Famous Arcade Game/04 - Write the Paddle Class and Create the Second Paddle/main.py
@@ -1,34 +1,26 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from paddle import Paddle
 
-paddle = Turtle()
+
 screen = Screen()
-
-
-def up():
-    ycor_up = paddle.position()[1] + 20
-    paddle.goto(x= 350, y= ycor_up)
-
-def down():
-    ycor_down = paddle.position()[1] - 20
-    paddle.goto(x= 350, y= ycor_down)
-
-
 screen.setup(height= 600, width= 800)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
 
-paddle.goto(x= 350, y=0)
-paddle.shape("square")
-paddle.penup()
-paddle.color("white")
-paddle.turtlesize(stretch_wid= 5, stretch_len=1)
+r_paddle = Paddle()
+l_paddle = Paddle()
+
+r_paddle.goto(x= 350, y= 0)
+l_paddle.goto(x= -350, y= 0)
 
 
 screen.listen()
-screen.onkeypress(up, "i")
-screen.onkeypress(down, "k")
+screen.onkeypress(r_paddle.up, "i")
+screen.onkeypress(r_paddle.down, "k")
+screen.onkeypress(l_paddle.up, "e")
+screen.onkeypress(l_paddle.down, "d")
 
 
 game_is_on = True
