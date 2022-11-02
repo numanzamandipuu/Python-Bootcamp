8663fc2d2c16d460ac68bc5c8107004bc89549f3
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-02 12:07:44 +0600

    pong, the famous arcade game final update

--- Day 22 - Intermediate - Build Pong - The Famous Arcade Game/09 - Score Keeping and Changing the Ball Speed/main.py
+++ Day 22 - Intermediate - Build Pong - The Famous Arcade Game/09 - Score Keeping and Changing the Ball Speed/main.py
@@ -25,26 +25,36 @@ screen.onkeypress(l_paddle.up, "e")
 screen.onkeypress(l_paddle.down, "d")
 
 
+time_sleep = 0.05
 game_is_on = True
+
 while game_is_on:
     
     screen.update()
-    time.sleep(0.05)
+    time.sleep(time_sleep)
 
 
     ball.move()
 
     if r_paddle.distance(ball) < 50 and ball.xcor() > 330 or l_paddle.distance(ball) < 50 and ball.xcor() < -330:
+
+        if time_sleep > 0:
+            time_sleep *= 0.9
+        
         ball.bounce_x()
 
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
     if ball.xcor() > 380:
+        scoreboard.r_increase()
         ball.reset_position()
+        time_sleep = 0.05
 
     if ball.xcor() < -380:
+        scoreboard.l_increase()
         ball.reset_position()
+        time_sleep = 0.05
 
 
 screen.exitonclick()
--- Day 22 - Intermediate - Build Pong - The Famous Arcade Game/09 - Score Keeping and Changing the Ball Speed/scoreboard.py
+++ Day 22 - Intermediate - Build Pong - The Famous Arcade Game/09 - Score Keeping and Changing the Ball Speed/scoreboard.py
@@ -8,7 +8,19 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
         self.goto(-100, 200)
         self.write(self.l_score, 80, font=("Courier", 60, "normal"))
         self.goto(100, 200)
         self.write(self.r_score, 80, font=("Courier", 60, "normal"))
+
+    def r_increase(self):
+        self.r_score += 1
+        self.update_scoreboard()
+
+    def l_increase(self):
+        self.l_score += 1
+        self.update_scoreboard()
