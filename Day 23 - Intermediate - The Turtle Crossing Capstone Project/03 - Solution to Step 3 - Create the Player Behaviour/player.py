161e9d99783259e3b4669a3b45974648f33dfb50
Numan Zaman Dipu <numanzamandipuu@gmail.com>
2022-11-08 23:52:03 +0600

    deleted def __init__(self)

--- Day 23 - Intermediate - The Turtle Crossing Capstone Project/03 - Solution to Step 3 - Create the Player Behaviour/player.py
+++ Day 23 - Intermediate - The Turtle Crossing Capstone Project/03 - Solution to Step 3 - Create the Player Behaviour/player.py
@@ -7,15 +7,3 @@ MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 class Player(Turtle):
-
-    def __init__(self):
-
-        super().__init__()
-
-        self.shape("turtle")
-
-        self.penup()
-
-        self.setheading(90)
-
-        self.goto(STARTING_POSITION)
