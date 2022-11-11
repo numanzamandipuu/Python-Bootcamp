5f1eeee26d2bed466b76006a7dbefe24f7d97753
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-11 11:02:55 +0600

    main.py and car_manager.py blueprint

--- Day 23 - Intermediate - The Turtle Crossing Capstone Project/03 - Solution to Step 3 - Create the Player Behaviour/main.py
+++ Day 23 - Intermediate - The Turtle Crossing Capstone Project/03 - Solution to Step 3 - Create the Player Behaviour/main.py
@@ -4,18 +4,11 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-screen.listen()
-
-tim = Player()
-car = CarManager()
-
-screen.onkeypress(fun=tim.up, key="w")
-screen.onkeypress(fun=tim.down, key="s")
 
 game_is_on = True
-
-screen.exitonclick()
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
