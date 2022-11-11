3020508ef5f970ea17c85254ea240719023e8280
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-11 11:14:18 +0600

    solution step 03 done

--- Day 23 - Intermediate - The Turtle Crossing Capstone Project/03 - Solution to Step 3 - Create the Player Behaviour/player.py
+++ Day 23 - Intermediate - The Turtle Crossing Capstone Project/03 - Solution to Step 3 - Create the Player Behaviour/player.py
@@ -1,5 +1,23 @@
 from turtle import Turtle
 
 
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
 class Player(Turtle):
-    pass
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def go_down(self):
+        self.backward(MOVE_DISTANCE)
