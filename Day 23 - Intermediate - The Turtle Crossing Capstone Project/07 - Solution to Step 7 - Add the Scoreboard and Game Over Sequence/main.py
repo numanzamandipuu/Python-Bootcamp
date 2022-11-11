daa8ad8f584663ae11b4a6170ba4a3cae4da8ac6
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-11 12:38:18 +0600

    solution to step 07 done

--- Day 23 - Intermediate - The Turtle Crossing Capstone Project/07 - Solution to Step 7 - Add the Scoreboard and Game Over Sequence/main.py
+++ Day 23 - Intermediate - The Turtle Crossing Capstone Project/07 - Solution to Step 7 - Add the Scoreboard and Game Over Sequence/main.py
@@ -11,6 +11,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -28,11 +29,14 @@ while game_is_on:
 
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
+            scoreboard.game_over()
             game_is_on = False
+
 
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_score()
 
 
 screen.exitonclick()
