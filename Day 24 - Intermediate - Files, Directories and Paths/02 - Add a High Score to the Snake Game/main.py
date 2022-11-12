4a21cfee18a3d6fbb2222e127b530058149725a3
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-13 00:23:20 +0600

    snake reset method working

--- Day 24 - Intermediate - Files, Directories and Paths/02 - Add a High Score to the Snake Game/main.py
+++ Day 24 - Intermediate - Files, Directories and Paths/02 - Add a High Score to the Snake Game/main.py
@@ -35,6 +35,7 @@ while game_is_on:
     #Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreboard.reset()
+        snake.reset()
 
     #Detect collision with tail.
     for segment in snake.segments:
@@ -42,6 +43,7 @@ while game_is_on:
             pass
         elif snake.head.distance(segment) < 10:
             scoreboard.reset()
+            snake.reset()
 
 
 
--- Day 24 - Intermediate - Files, Directories and Paths/02 - Add a High Score to the Snake Game/snake.py
+++ Day 24 - Intermediate - Files, Directories and Paths/02 - Add a High Score to the Snake Game/snake.py
@@ -28,6 +28,13 @@ class Snake:
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
