779ddd240f4e4e419ad996bc2dd5827c10660e6f
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-13 19:26:25 +0600

    updated data.txt in main.py of snake game

--- Day 24 - Intermediate - Files, Directories and Paths/04 - Challenge - Read and Write the High Score to a File in Snake/scoreboard.py
+++ Day 24 - Intermediate - Files, Directories and Paths/04 - Challenge - Read and Write the High Score to a File in Snake/scoreboard.py
@@ -8,7 +8,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("D:/Python-Bootcamp/Day 24 - Intermediate - Files, Directories and Paths/04 - Challenge - Read and Write the High Score to a File in Snake/data.txt", mode= "r") as file:
+            high_score_int = int(file.read())
+        self.high_score = high_score_int
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,6 +24,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.high_score < self.score:
             self.high_score = self.score
+            with open("D:/Python-Bootcamp/Day 24 - Intermediate - Files, Directories and Paths/04 - Challenge - Read and Write the High Score to a File in Snake/data.txt", mode= "w") as files:
+                files.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
 
