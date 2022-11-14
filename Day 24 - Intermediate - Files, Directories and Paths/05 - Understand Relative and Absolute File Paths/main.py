b26e57627cff4a28b7be7a645922b5206580fb32
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-14 10:29:24 +0600

    relative and absolute file path final

--- Day 24 - Intermediate - Files, Directories and Paths/05 - Understand Relative and Absolute File Paths/main.py
+++ Day 24 - Intermediate - Files, Directories and Paths/05 - Understand Relative and Absolute File Paths/main.py
@@ -1,6 +1,3 @@
 with open("D:/Python-Bootcamp/Day 24 - Intermediate - Files, Directories and Paths/03 - How to Open, Read, and Write to Files using the _with_ Keyword/main.txt") as file:
     content = file.read()
     print(content)
-
-with open("D:/Python-Bootcamp/Day 24 - Intermediate - Files, Directories and Paths/03 - How to Open, Read, and Write to Files using the _with_ Keyword/new_file.txt", mode="w") as new_file:
-    new_file.write("Hello Again.")
