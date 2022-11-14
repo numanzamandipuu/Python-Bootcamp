22c5e7b2ff8464a5b6d330b860d88d47d90a77e5
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-14 11:15:27 +0600

    mail merge challenge done (my code)

--- Day 24 - Intermediate - Files, Directories and Paths/06 - Introducing the Mail Merge Challenge/main.py
+++ Day 24 - Intermediate - Files, Directories and Paths/06 - Introducing the Mail Merge Challenge/main.py
@@ -1,8 +1,12 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-#Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-#Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+with open("D:/Python-Bootcamp/Day 24 - Intermediate - Files, Directories and Paths/06 - Introducing the Mail Merge Challenge/Input/Names/invited_names.txt") as invited_names:
+    names = invited_names.readlines()
+    for i in names:
+        i = i.strip()
+
+        with open("D:/Python-Bootcamp/Day 24 - Intermediate - Files, Directories and Paths/06 - Introducing the Mail Merge Challenge/Input/Letters/starting_letter.txt") as starting_letter:
+            first_line = starting_letter.readlines()
+            for line in first_line:
+                line = line.strip()
+                new_line = line.replace("[name]", i)
+                with open(f"D:/Python-Bootcamp/Day 24 - Intermediate - Files, Directories and Paths/06 - Introducing the Mail Merge Challenge/Output/ReadyToSend/letter_for_{i}.txt", mode= "a") as new_letter:
+                    new_letter.write(f"{new_line}\n")
