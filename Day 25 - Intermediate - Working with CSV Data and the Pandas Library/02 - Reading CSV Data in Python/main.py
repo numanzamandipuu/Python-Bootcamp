573670d1a28a39b7346c3611940d3f6104ff84b2
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-15 18:57:17 +0600

    challenge for the temp row (my code)

---
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/main.py
@@ -0,0 +1,17 @@
+# with open("D:/Python-Bootcamp/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/weather-data.csv") as file:
+#     data = file.readlines()
+#     print(data)
+
+import csv
+
+with open("D:/Python-Bootcamp/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/weather-data.csv") as file:
+    data = csv.reader(file)
+    temperature = []
+
+    for i in data:
+        if i[1] == "temp":
+            pass
+        else:
+            temperature.append(int(i[1]))
+
+    print(temperature)
