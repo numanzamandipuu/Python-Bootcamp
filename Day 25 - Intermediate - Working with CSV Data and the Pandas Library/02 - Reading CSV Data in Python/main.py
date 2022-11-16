5f009a4c5e5d6ddd4513e76bc143540295426b00
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-16 09:27:21 +0600

    read_csv and printing out the csv file

--- Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/main.py
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/main.py
@@ -17,4 +17,7 @@
 #     print(temperature)
 
 
-import pandas
+import pandas 
+
+data = pandas.read_csv("D:/Python-Bootcamp/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/weather-data.csv")
+print(data)
