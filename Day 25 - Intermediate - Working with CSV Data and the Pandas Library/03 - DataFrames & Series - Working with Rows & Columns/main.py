573cda317116eeb8ee059e75dff5e780baa8e41b
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-16 09:35:57 +0600

    type check of pandas DataFrame & Series

--- Day 25 - Intermediate - Working with CSV Data and the Pandas Library/03 - DataFrames & Series - Working with Rows & Columns/main.py
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/03 - DataFrames & Series - Working with Rows & Columns/main.py
@@ -2,5 +2,5 @@ import pandas
 
 data = pandas.read_csv("D:/Python-Bootcamp/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/weather-data.csv")
 
-print(data)
-print(data["temp"])
+print(type(data))
+print(type(data["temp"]))
