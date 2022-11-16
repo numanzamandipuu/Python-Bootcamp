5859ce7278a4ea2c88724f1f2dc8f64438a5cc51
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-16 09:34:42 +0600

    deleted the commented out unnecessary codes

--- Day 25 - Intermediate - Working with CSV Data and the Pandas Library/03 - DataFrames & Series - Working with Rows & Columns/main.py
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/03 - DataFrames & Series - Working with Rows & Columns/main.py
@@ -1,22 +1,3 @@
-# with open("D:/Python-Bootcamp/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/weather-data.csv") as file:
-#     data = file.readlines()
-#     print(data)
-
-# import csv
-
-# with open("D:/Python-Bootcamp/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/weather-data.csv") as file:
-#     data = csv.reader(file)
-#     temperature = []
-
-#     for i in data:
-#         if i[1] == "temp":
-#             pass
-#         else:
-#             temperature.append(int(i[1]))
-
-#     print(temperature)
-
-
 import pandas 
 
 data = pandas.read_csv("D:/Python-Bootcamp/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/weather-data.csv")
