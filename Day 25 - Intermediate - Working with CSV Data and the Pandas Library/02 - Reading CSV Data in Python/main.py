c6725253f57dfbe16f4a90dcba348e955e932e3a
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-16 09:24:34 +0600

    commented out csv module, install & import pandas

--- Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/main.py
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/main.py
@@ -2,16 +2,19 @@
 #     data = file.readlines()
 #     print(data)
 
-import csv
+# import csv
 
-with open("D:/Python-Bootcamp/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/weather-data.csv") as file:
-    data = csv.reader(file)
-    temperature = []
+# with open("D:/Python-Bootcamp/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/weather-data.csv") as file:
+#     data = csv.reader(file)
+#     temperature = []
 
-    for i in data:
-        if i[1] == "temp":
-            pass
-        else:
-            temperature.append(int(i[1]))
+#     for i in data:
+#         if i[1] == "temp":
+#             pass
+#         else:
+#             temperature.append(int(i[1]))
 
-    print(temperature)
+#     print(temperature)
+
+
+import pandas
