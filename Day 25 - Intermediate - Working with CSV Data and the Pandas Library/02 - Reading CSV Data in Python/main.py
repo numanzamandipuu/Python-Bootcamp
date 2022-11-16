d6f75ed6742aced2d8261855d8fc0d0c67d36ca1
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-16 09:28:29 +0600

    printing the temp column

--- Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/main.py
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/main.py
@@ -20,4 +20,6 @@
 import pandas 
 
 data = pandas.read_csv("D:/Python-Bootcamp/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/weather-data.csv")
+
 print(data)
+print(data["temp"])
