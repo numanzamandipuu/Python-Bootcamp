1569ffa73ab15431d4218764866c1ec94fe5d8ae
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-16 09:55:00 +0600

    added comments to individual segment

--- Day 25 - Intermediate - Working with CSV Data and the Pandas Library/03 - DataFrames & Series - Working with Rows & Columns/main.py
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/03 - DataFrames & Series - Working with Rows & Columns/main.py
@@ -2,15 +2,23 @@ import pandas
 
 data = pandas.read_csv("D:/Python-Bootcamp/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/02 - Reading CSV Data in Python/weather-data.csv")
 
+
+# getting the data type
 print(type(data))
 print(type(data["temp"]))
 
+
+# creating a dictionary
 data_dict = data.to_dict()
 print(data_dict)
 
+
+# creating a list
 temp_list = data["temp"].to_list()
 print(temp_list)
 
+
+# getting the avg of temp_list
 total = 0
 for i in data["temp"]:
     total += i
