efde66c39fba8409f9cc18b1897805b04d9bc521
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-18 19:57:21 +0600

    main.py blueprint for part 03

---
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/07 - U.S. States Game Part 3 - Saving Data to .csv/main.py
@@ -0,0 +1,37 @@
+import turtle
+import pandas
+
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+img_path = "D:/Python-Bootcamp/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/05 - U.S. States Game Part 1 - Setup/blank_states_img.gif"
+
+
+turtle.bgpic(img_path)
+turtle.hideturtle()
+turtle.penup()
+correct_ans = 0
+correct_guess = []
+
+
+data = pandas.read_csv("D:/Python-Bootcamp/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/05 - U.S. States Game Part 1 - Setup/50_states.csv")
+state_list = data["state"].tolist()
+
+
+while len(correct_guess) < 50:
+
+    cap_answer = screen.textinput(title= f"{correct_ans}/50 States Correct", prompt= "What's another state's name?")
+    answer = cap_answer.title()
+
+    if answer in state_list:
+        if answer in correct_guess:
+            pass
+        else:
+            ans_row = data[data.state == answer]
+            turtle.setposition(int(ans_row["x"]), int(ans_row["y"]))
+            turtle.write(answer)
+            correct_ans += 1
+            correct_guess.append(answer)
+
+
+screen.exitonclick()
