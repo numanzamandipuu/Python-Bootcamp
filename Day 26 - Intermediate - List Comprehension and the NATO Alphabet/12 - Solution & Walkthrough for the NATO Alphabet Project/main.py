c2b43f96c52413572cdf042cdaf6f0f931949245
Numan Zaman Dipu <numanzamandipuu@gmail.com>
2022-11-22 23:57:00 +0600

    deleted the upper comments

--- Day 26 - Intermediate - List Comprehension and the NATO Alphabet/12 - Solution & Walkthrough for the NATO Alphabet Project/main.py
+++ Day 26 - Intermediate - List Comprehension and the NATO Alphabet/12 - Solution & Walkthrough for the NATO Alphabet Project/main.py
@@ -3,22 +3,16 @@ student_dict = {
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
+
 for (key, value) in student_dict.items():
-    #Access key and value
     pass
 
 import pandas
 student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+for (index, row) in student_data_frame.iterrows():
+    pass
 
 #TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
