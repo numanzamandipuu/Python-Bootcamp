a77c11ffd08ce8ed5de8bb27870a243050440270
Numan Zaman Dipu <numanzamandipuu@gmail.com>
2022-11-22 23:59:14 +0600

    deleted looping and pass comments

--- Day 26 - Intermediate - List Comprehension and the NATO Alphabet/11 - Introducing the NATO Alphabet Project/main.py
+++ Day 26 - Intermediate - List Comprehension and the NATO Alphabet/11 - Introducing the NATO Alphabet Project/main.py
@@ -3,18 +3,13 @@ student_dict = {
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
 for (key, value) in student_dict.items():
-    #Access key and value
     pass
 
 import pandas
 student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
     pass
 
 # Keyword Method with iterrows()
