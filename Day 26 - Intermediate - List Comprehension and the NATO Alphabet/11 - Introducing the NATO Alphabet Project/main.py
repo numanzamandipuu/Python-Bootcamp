9bef8416ece7245f1da6907bcba395de9cd761d2
Numan Zaman Dipu <numanzamandipuu@gmail.com>
2022-11-22 23:59:42 +0600

    deleted existing todo comments

--- Day 26 - Intermediate - List Comprehension and the NATO Alphabet/11 - Introducing the NATO Alphabet Project/main.py
+++ Day 26 - Intermediate - List Comprehension and the NATO Alphabet/11 - Introducing the NATO Alphabet Project/main.py
@@ -12,11 +12,4 @@ student_data_frame = pandas.DataFrame(student_dict)
 for (index, row) in student_data_frame.iterrows():
     pass
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
 {"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
