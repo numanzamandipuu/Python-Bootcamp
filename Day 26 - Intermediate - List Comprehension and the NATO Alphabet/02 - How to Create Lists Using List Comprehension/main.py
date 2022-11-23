ad54db699e91a7ce2a5edb20e44dd65cadd0848c
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-23 23:43:49 +0600

    list comprehension for short/long names

--- Day 26 - Intermediate - List Comprehension and the NATO Alphabet/02 - How to Create Lists Using List Comprehension/main.py
+++ Day 26 - Intermediate - List Comprehension and the NATO Alphabet/02 - How to Create Lists Using List Comprehension/main.py
@@ -8,3 +8,11 @@ print(letters_list)
 
 range_list = [num * 2 for num in range(1, 5)]
 print(range_list)
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
+
+short_names = [name for name in names if len(name) < 5]
+print(short_names)
+
+long_names = [name.upper() for name in names if len(name) > 5]
+print(long_names)
