b9504adfa75696630f9b2396857cf2942c13aad5
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-24 10:38:20 +0600

    dictionary comprehension update

--- Day 26 - Intermediate - List Comprehension and the NATO Alphabet/07 - How to Use Dictionary Comprehension/main.py
+++ Day 26 - Intermediate - List Comprehension and the NATO Alphabet/07 - How to Use Dictionary Comprehension/main.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+import random
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Elanor", "Freddie"]
+
+students_score = {s : random.randint(1, 100) for s in names}
+print(students_score)
+
+passed_students = {students : score for (students, score) in students_score.items() if score >= 60}
+print(passed_students)
