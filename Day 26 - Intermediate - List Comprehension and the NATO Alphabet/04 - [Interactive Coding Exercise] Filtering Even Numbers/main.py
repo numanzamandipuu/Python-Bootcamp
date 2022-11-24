b70f1f32132c8448be327710d9fdfad5dfdbca94
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-24 11:07:51 +0600

    interactive even numbers

--- Day 26 - Intermediate - List Comprehension and the NATO Alphabet/04 - [Interactive Coding Exercise] Filtering Even Numbers/main.py
+++ Day 26 - Intermediate - List Comprehension and the NATO Alphabet/04 - [Interactive Coding Exercise] Filtering Even Numbers/main.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# 🚨 Do Not Change the code above
+
+#Write your 1 line code 👇 below:
+
+result = [n for n in numbers if n % 2 == 0]
+
+#Write your code 👆 above:
+
+print(result)
