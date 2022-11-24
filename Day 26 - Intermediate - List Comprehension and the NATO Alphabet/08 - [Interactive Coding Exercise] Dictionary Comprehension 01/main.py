88e1d57ebda4decb80711f70e5b6011daf301f7d
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-24 11:14:17 +0600

    interactive dictionary comprehension part 01

--- Day 26 - Intermediate - List Comprehension and the NATO Alphabet/08 - [Interactive Coding Exercise] Dictionary Comprehension 01/main.py
+++ Day 26 - Intermediate - List Comprehension and the NATO Alphabet/08 - [Interactive Coding Exercise] Dictionary Comprehension 01/main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+
+# Write your code below:
+
+words = sentence.split()
+result = {
+    word: len(word) for word in words
+}
+
+print(result)
