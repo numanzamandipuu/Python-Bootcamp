17d30f8f253faaba6391a8bbc4eadaec3aee4f43
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-24 11:44:13 +0600

    iterate over pandas dataframe

--- Day 26 - Intermediate - List Comprehension and the NATO Alphabet/10 - How to Iterate Over a Pandas DataFrame/main.py
+++ Day 26 - Intermediate - List Comprehension and the NATO Alphabet/10 - How to Iterate Over a Pandas DataFrame/main.py
@@ -1,1 +1,24 @@
-print("Hello World!")
+import pandas
+
+student_dict = {
+    "student" : ["dipu", "angela", "yu"],
+    "score" : [56, 76, 98]
+}
+
+# looping through dictionary 
+for (key, value) in student_dict.items():
+    print(key)
+    print(value)
+
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
+
+# looping through a dataframe
+for (key, value) in student_data_frame.items():
+    print(key)
+    print(value)
+
+# looping through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    print(index)
+    print(row)
