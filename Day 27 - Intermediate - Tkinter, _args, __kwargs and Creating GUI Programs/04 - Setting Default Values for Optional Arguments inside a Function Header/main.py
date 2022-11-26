8baf069f18721b36395f4997bbe5afcaa0fc772c
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-26 09:52:10 +0600

    default values for optional arguments

--- Day 27 - Intermediate - Tkinter, _args, __kwargs and Creating GUI Programs/04 - Setting Default Values for Optional Arguments inside a Function Header/main.py
+++ Day 27 - Intermediate - Tkinter, _args, __kwargs and Creating GUI Programs/04 - Setting Default Values for Optional Arguments inside a Function Header/main.py
@@ -1,11 +1,16 @@
 import tkinter
+import turtle
+
 
 window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width= 500, height= 300)
 
 label = tkinter.Label(text= "I'm a Label", font= ("Ariel", 20))
-label.pack()
+label.pack(side= "left")
+
+tim = turtle.Turtle()
+tim.write("some text", font= ("Times New Roman", 80, "bold"))
 
 
 window.mainloop()
