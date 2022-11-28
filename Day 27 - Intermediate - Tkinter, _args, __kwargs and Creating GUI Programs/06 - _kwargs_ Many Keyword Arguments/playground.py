df3df9592bc0c2278bd2913ccbd7e7826bcb0094
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-28 09:59:18 +0600

    playground.py final for kwargs

--- Day 27 - Intermediate - Tkinter, _args, __kwargs and Creating GUI Programs/06 - _kwargs_ Many Keyword Arguments/playground.py
+++ Day 27 - Intermediate - Tkinter, _args, __kwargs and Creating GUI Programs/06 - _kwargs_ Many Keyword Arguments/playground.py
@@ -5,3 +5,16 @@ def add(*args):
     return a
 
 print(add(3, 4, 5, 6, 7, 8, 9))
+
+def calculate(n, **kwargs):
+    print(kwargs)
+
+    for key, value in kwargs.items():
+        print(key)
+        print(value)
+
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+calculate(2, add= 3, multiply= 5)
