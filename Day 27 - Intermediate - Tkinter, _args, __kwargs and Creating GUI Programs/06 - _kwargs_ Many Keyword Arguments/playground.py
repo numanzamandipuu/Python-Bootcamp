ce240115281001003d7ee8636117aadeb160f69c
numanzamandipuu <numanzamandipuu@gmail.com>
2022-11-28 10:39:34 +0600

    class added to playground.py

--- Day 27 - Intermediate - Tkinter, _args, __kwargs and Creating GUI Programs/06 - _kwargs_ Many Keyword Arguments/playground.py
+++ Day 27 - Intermediate - Tkinter, _args, __kwargs and Creating GUI Programs/06 - _kwargs_ Many Keyword Arguments/playground.py
@@ -18,3 +18,16 @@ def calculate(n, **kwargs):
     print(n)
 
 calculate(2, add= 3, multiply= 5)
+
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+my_car = Car(make= "Nissan")
+print(my_car.make)
+print(my_car.model)
