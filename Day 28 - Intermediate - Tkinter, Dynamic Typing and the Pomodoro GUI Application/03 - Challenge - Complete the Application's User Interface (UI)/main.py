effec06d365b95eb0813a1758115898a9b670a56
numanzamandipuu <numanzamandipuu@gmail.com>
2022-12-02 11:41:25 +0600

    application's user interface (my code)

--- Day 28 - Intermediate - Tkinter, Dynamic Typing and the Pomodoro GUI Application/03 - Challenge - Complete the Application's User Interface (UI)/main.py
+++ Day 28 - Intermediate - Tkinter, Dynamic Typing and the Pomodoro GUI Application/03 - Challenge - Complete the Application's User Interface (UI)/main.py
@@ -21,14 +21,25 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx= 80, pady= 40, bg= YELLOW)
+window.config(padx= 80, pady= 50, bg= YELLOW)
+
+label = Label(text= "Timer", font= (FONT_NAME, 35, "bold"), bg= YELLOW, highlightthickness= 0, fg= GREEN)
+label.grid(column= 2, row= 1)
 
 canvas = Canvas(width= 250, height= 270, bg= YELLOW, highlightthickness= 0)
-tomato_img = PhotoImage(file= "D:/Python-Bootcamp/Day 28 - Intermediate - Tkinter, Dynamic Typing and the Pomodoro GUI Application/02 - How to work with the Canvas Widget and Add Images to Tkinter/tomato.png")
+tomato_img = PhotoImage(file= "D:/Python-Bootcamp/Day 28 - Intermediate - Tkinter, Dynamic Typing and the Pomodoro GUI Application/03 - Challenge - Complete the Application's User Interface (UI)/tomato.png")
 canvas.create_image(125, 130, image= tomato_img)
 canvas.create_text(125, 150, text= "00:00", fill= "white", font= (FONT_NAME, 35, "bold"))
+canvas.grid(column= 2, row= 2)
 
-canvas.pack()
+button01 = Button(text= "Start")
+button01.grid(column= 1, row= 3)
+
+button02 = Button(text= "Reset")
+button02.grid(column= 3, row= 3)
+
+tick_label = Label(text= "✔", bg= YELLOW, fg= GREEN, font= (FONT_NAME, 25, "bold"))
+tick_label.grid(column= 2, row= 4)
 
 
 
