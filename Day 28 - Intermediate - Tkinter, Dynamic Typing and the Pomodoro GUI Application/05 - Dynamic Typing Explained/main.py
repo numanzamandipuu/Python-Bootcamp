dd44daaf6162a2f67c8474cf9fee0a9c02ea2354
numanzamandipuu <numanzamandipuu@gmail.com>
2022-12-03 12:58:40 +0600

    dynamic typing explained

--- Day 28 - Intermediate - Tkinter, Dynamic Typing and the Pomodoro GUI Application/05 - Dynamic Typing Explained/main.py
+++ Day 28 - Intermediate - Tkinter, Dynamic Typing and the Pomodoro GUI Application/05 - Dynamic Typing Explained/main.py
@@ -29,6 +29,9 @@ def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
 
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
     canvas.itemconfig(timer_text, text= f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
