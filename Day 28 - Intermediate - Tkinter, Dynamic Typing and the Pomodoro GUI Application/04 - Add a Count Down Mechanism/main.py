7a7716cded9dd2b0d838303eaddd415c03895830
numanzamandipuu <numanzamandipuu@gmail.com>
2022-12-03 12:21:15 +0600

    count down mechanism final

--- Day 28 - Intermediate - Tkinter, Dynamic Typing and the Pomodoro GUI Application/04 - Add a Count Down Mechanism/main.py
+++ Day 28 - Intermediate - Tkinter, Dynamic Typing and the Pomodoro GUI Application/04 - Add a Count Down Mechanism/main.py
@@ -1,7 +1,9 @@
 from tkinter import *
+import math
 
 
 # ---------------------------- CONSTANTS ------------------------------- #
+
 PINK = "#E2979C"
 RED = "#E7305B"
 GREEN = "#9BFEAC"
@@ -11,18 +13,26 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def timer_start():
-    count_down(5)
+    count_down(5 * 60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
 def count_down(count):
-    canvas.itemconfig(timer_text, text= count)
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text= f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
