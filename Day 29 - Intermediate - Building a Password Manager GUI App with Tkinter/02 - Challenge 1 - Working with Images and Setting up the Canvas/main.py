0976cdd829e38fbfaf0275158b1bbc61d529f2af
numanzamandipuu <numanzamandipuu@gmail.com>
2022-12-07 21:58:49 +0600

    image updated to GUI program

--- Day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/02 - Challenge 1 - Working with Images and Setting up the Canvas/main.py
+++ Day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/02 - Challenge 1 - Working with Images and Setting up the Canvas/main.py
@@ -1,5 +1,19 @@
+from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password")
+window.config(padx= 20, pady= 20)
+
+canvas = Canvas(height= 200, width= 200)
+img_path = PhotoImage(file= "D:/Python-Bootcamp/Day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/02 - Challenge 1 - Working with Images and Setting up the Canvas/logo.png")
+canvas.create_image(100, 100, image= img_path)
+canvas.pack()
+
+window.mainloop()
