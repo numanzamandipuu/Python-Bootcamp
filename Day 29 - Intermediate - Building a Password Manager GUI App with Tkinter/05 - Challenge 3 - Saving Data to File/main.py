a5cec43042d784f388bcc5bb6272a97f28ba25e2
numanzamandipuu <numanzamandipuu@gmail.com>
2022-12-09 11:49:32 +0600

    saving data to file update (my code)

--- Day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/05 - Challenge 3 - Saving Data to File/main.py
+++ Day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/05 - Challenge 3 - Saving Data to File/main.py
@@ -5,6 +5,19 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    website = entry_01.get()
+    email = entry_02.get()
+    password = entry_03.get()
+    entry_01.delete(0, END)
+    entry_02.delete(0, END)
+    entry_03.delete(0, END)
+    entry_02.insert(0, "numanzamandipuu@gmail.com")
+
+    with open("D:/Python-Bootcamp/Day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/05 - Challenge 3 - Saving Data to File/data.txt", "a") as file:
+        file.write(f"\n{website} | {email} | {password}")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -27,15 +40,17 @@ label_03.config(padx= 5, pady= 5)
 label_03.grid(row= 3, column= 0, sticky= "w")
 
 entry_01 = Entry(width= 70)
+entry_01.focus()
 entry_01.grid(row= 1, column= 1, columnspan= 2, sticky= "w")
 entry_02 = Entry(width= 70)
+entry_02.insert(0, "numanzamandipuu@gmail.com")
 entry_02.grid(row= 2, column= 1, columnspan= 2, sticky= "w")
 entry_03 = Entry(width= 38)
 entry_03.grid(row= 3, column= 1, sticky= "w")
 
 button_01 = Button(text= "Generate Password", width= 25)
 button_01.grid(row= 3, column= 2)
-button_02 = Button(text= "Add", width= 59)
+button_02 = Button(text= "Add", width= 59, command= save)
 button_02.grid(row= 4, column= 1, columnspan= 2)
 
 
