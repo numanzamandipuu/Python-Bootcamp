86e7329e41b5f63107e4c6c02fae5086f6099f04
numanzamandipuu <numanzamandipuu@gmail.com>
2022-12-10 20:11:15 +0600

    catching exceptions first part

---
+++ Day 30 - Intermediate - Errors, Exceptions and JSON Data - Improving the Password/02 - Catching Exceptions - The Try Catch Except Finally Pattern/main.py
@@ -0,0 +1,15 @@
+# FileNotFoundError
+# with open("file.txt") as file:
+#     file.read()
+
+# KeyError
+# a_dictionary = {"key" : "value"}
+# value = a_dictionary["non_existing_key"]
+
+# IndexError
+# fruit_list = ["Apple", "Banana", "Pear"]
+# fruit = fruit_list[3]
+
+# TypeError
+# text = "abc"
+# print(text + 5)
