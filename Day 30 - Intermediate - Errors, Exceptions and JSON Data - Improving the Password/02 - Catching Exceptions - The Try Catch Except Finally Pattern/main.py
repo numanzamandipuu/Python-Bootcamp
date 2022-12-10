15c5e6d0e0d116ce59a7363932f13614a4e34245
numanzamandipuu <numanzamandipuu@gmail.com>
2022-12-10 20:20:03 +0600

    catching exception done

--- Day 30 - Intermediate - Errors, Exceptions and JSON Data - Improving the Password/02 - Catching Exceptions - The Try Catch Except Finally Pattern/main.py
+++ Day 30 - Intermediate - Errors, Exceptions and JSON Data - Improving the Password/02 - Catching Exceptions - The Try Catch Except Finally Pattern/main.py
@@ -13,3 +13,23 @@
 # TypeError
 # text = "abc"
 # print(text + 5)
+
+try:
+    file = open("D:/Python-Bootcamp/Day 30 - Intermediate - Errors, Exceptions and JSON Data - Improving the Password/02 - Catching Exceptions - The Try Catch Except Finally Pattern/file.txt")
+    a_dictionary = {"key" : "value"}
+    print(a_dictionary["key"])
+
+except FileNotFoundError:
+    file = open("D:/Python-Bootcamp/Day 30 - Intermediate - Errors, Exceptions and JSON Data - Improving the Password/02 - Catching Exceptions - The Try Catch Except Finally Pattern/file.txt", "w")
+    file.write("Something")
+
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist")
+
+else:
+    content = file.read()
+    print(content)
+
+finally:
+    file.close()
+    print("File was closed.")
