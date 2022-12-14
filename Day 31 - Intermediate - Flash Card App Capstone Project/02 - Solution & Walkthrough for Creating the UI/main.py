5e65d8769e09ebc4bb59a958e68be2e59062d33d
numanzamandipuu <numanzamandipuu@gmail.com>
2022-12-14 21:34:30 +0600

    user interface setup (my code)

--- Day 31 - Intermediate - Flash Card App Capstone Project/02 - Solution & Walkthrough for Creating the UI/main.py
+++ Day 31 - Intermediate - Flash Card App Capstone Project/02 - Solution & Walkthrough for Creating the UI/main.py
@@ -7,28 +7,27 @@ RIGHT = "D:/Python-Bootcamp/Day 31 - Intermediate - Flash Card App Capstone Proj
 WRONG = "D:/Python-Bootcamp/Day 31 - Intermediate - Flash Card App Capstone Project/02 - Solution & Walkthrough for Creating the UI/wrong.png"
 
 
+# ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Flashy")
 window.config(padx= 100, pady= 50)
-
-
 
 front_img = PhotoImage(file= FRONT)
 back_img = PhotoImage(file= BACK)
 right_img = PhotoImage(file= RIGHT)
 wrong_img = PhotoImage(file= WRONG)
 
-
 canvas = Canvas(width= 450, height= 300)
 canvas.create_image(225, 150, image= back_img)
+canvas.create_text(225, 115, text= "Title", font= ("Ariel", 25, "italic"))
+canvas.create_text(225, 175, text= "Word", font= ("Ariel", 40, "bold"))
 canvas.grid(row= 1, column= 1, columnspan= 2)
 
 right_button = Button(image= right_img, highlightthickness= 0)
 right_button.grid(row= 2, column= 1)
 wrong_button = Button(image= wrong_img, highlightthickness= 0)
 wrong_button.grid(row= 2, column= 2)
-
-
 
 
 window.mainloop()
