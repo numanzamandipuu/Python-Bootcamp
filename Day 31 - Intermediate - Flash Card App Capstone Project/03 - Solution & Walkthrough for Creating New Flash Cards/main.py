39b3a5bfb5980ef1569085eb03cad7977f8bd18d
numanzamandipuu <numanzamandipuu@gmail.com>
2022-12-14 23:19:30 +0600

    modified the new flash card

--- Day 31 - Intermediate - Flash Card App Capstone Project/03 - Solution & Walkthrough for Creating New Flash Cards/main.py
+++ Day 31 - Intermediate - Flash Card App Capstone Project/03 - Solution & Walkthrough for Creating New Flash Cards/main.py
@@ -20,6 +20,7 @@ def flash_card():
     index = random.randint(0, 499)
     word = data[index]["ENGLISH"]
     canvas.itemconfig(word_text, text= word)
+    canvas.itemconfig(title_text, text= "English")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -35,14 +36,16 @@ wrong_img = PhotoImage(file= WRONG)
 
 canvas = Canvas(width= 450, height= 300, bg= BACKGROUND_COLOR, highlightthickness= 0)
 canvas.create_image(225, 150, image= back_img)
-title_text = canvas.create_text(225, 115, text= "Title", font= ("Ariel", 25, "italic"))
-word_text = canvas.create_text(225, 175, text= "Word", font= ("Ariel", 40, "bold"))
+title_text = canvas.create_text(225, 115, text= "", font= ("Ariel", 25, "italic"))
+word_text = canvas.create_text(225, 175, text= "", font= ("Ariel", 40, "bold"))
 canvas.grid(row= 1, column= 1, columnspan= 2)
 
 right_button = Button(image= right_img, highlightthickness= 0, command= flash_card)
 right_button.grid(row= 2, column= 1)
 wrong_button = Button(image= wrong_img, highlightthickness= 0, command= flash_card)
 wrong_button.grid(row= 2, column= 2)
+
+flash_card()
 
 
 window.mainloop()
